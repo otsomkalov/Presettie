5e887ebdee8abcfa84651c0a999debaa1b238ee4
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-12-26 16:17:42 +0200

    Add API project with basic auth & User presets listing endpoint

---
+++ src/API/Functions/AuthFunctions.fs
@@ -0,0 +1,23 @@
+﻿namespace API.Functions
+
+open System.Threading.Tasks
+open API.Services
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Azure.Functions.Worker
+open Microsoft.Azure.Functions.Worker.Http
+open otsom.fs.Core
+
+[<CLIMutable>]
+type TokenRequest = { UserId: int64 }
+
+type AuthFunctions(jwtService: IJWTService) =
+
+  [<Function("GetToken")>]
+  member this.GetToken
+    ([<HttpTrigger(AuthorizationLevel.Anonymous, "POST", Route = "auth/token")>] request: HttpRequest, [<FromBody>] body: TokenRequest)
+    =
+
+    let token = jwtService.GenerateToken(UserId body.UserId)
+
+    OkObjectResult token :> IActionResult |> Task.FromResult
---
+++ src/API/Functions/PresetFunctions.fs
@@ -0,0 +1,37 @@
+﻿namespace API.Functions
+
+open System.Threading.Tasks
+open API.Services
+open Domain.Core
+open Domain.Repos
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Azure.Functions.Worker
+open otsom.fs.Extensions
+
+type PresetFunctions(userRepo: IUserRepo, jwtService: IJWTService) =
+
+  let runForUser (req: HttpRequest) (fn) =
+    req.Headers.Authorization
+    |> string
+    |> Option.ofObj
+    |> Option.map (_.Split(" "))
+    |> Option.bind (function
+      | [| "Bearer"; token |] -> Some(token)
+      | _ -> None)
+    |> Option.taskMap (jwtService.DecodeToken)
+    |> Task.map Option.flatten
+    |> TaskOption.taskMap fn
+    |> Task.bind (Option.defaultWithTask (fun () -> UnauthorizedResult() :> IActionResult |> Task.FromResult))
+
+  [<Function("ListPresets")>]
+  member this.ListPresets
+    ([<HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "presets")>] request: HttpRequest)
+    : Task<IActionResult> =
+    let handler (user: TokenUser) = task {
+      let! user = userRepo.LoadUser user.Id
+
+      return OkObjectResult(user.Presets) :> IActionResult
+    }
+
+    runForUser request handler
