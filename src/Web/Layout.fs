dece06096ee61432e17171290d754c8e78f3abbd
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-07-29 23:22:42 +0300

    Add Web project

---
+++ src/Web/Layout.fs
@@ -0,0 +1,63 @@
+﻿[<RequireQualifiedAccess>]
+module Web.Layout
+
+open Bolero.Html
+
+module Header =
+  let view dispatch = nav {
+    attr.``class`` "navbar bg-primary navbar-expand-lg"
+    "data-bs-theme" => "dark"
+
+    div {
+      attr.``class`` "container-fluid"
+
+      a {
+        attr.``class`` "navbar-brand"
+        attr.href "/"
+        "Presettie"
+      }
+
+      button {
+        attr.``class`` "navbar-toggler"
+        attr.``type`` "button"
+        "data-bs-toggle" => "collapse"
+        "data-bs-target" => "#navbarNavDropdown"
+        "aria-controls" => "navbarNavDropdown"
+        "aria-expanded" => "false"
+        "aria-label" => "Toggle navigation"
+
+        span { attr.``class`` "navbar-toggler-icon" }
+      }
+
+      div {
+        attr.``class`` "collapse navbar-collapse"
+        attr.id "navbarNavDropdown"
+
+        ul {
+          attr.``class`` "navbar-nav"
+
+          li {
+            attr.``class`` "nav-item"
+
+            a {
+              attr.``class`` "nav-link"
+              attr.href "/"
+
+              "Home"
+            }
+          }
+
+          li {
+            attr.``class`` "nav-item"
+
+            a {
+              attr.``class`` "nav-link"
+              attr.href "presets"
+
+              "Presets"
+            }
+          }
+        }
+      }
+    }
+  }
---
+++ src/Web/Main.fs
@@ -0,0 +1,56 @@
+﻿module Web.Main
+
+open Elmish
+open Bolero
+open Bolero.Html
+
+[<RequireQualifiedAccess>]
+type Page =
+  | [<EndPoint("/")>] Home
+  | [<EndPoint("**")>] Loading
+
+type Model = { Page: Page }
+
+let initModel = { Page = Page.Loading }, Cmd.none
+
+type Message = PageChanged of Page
+
+let update (message: Message) (model: Model) =
+  match message, model.Page with
+  | PageChanged(Page.Home), _ -> { model with Page = Page.Home }, Cmd.none
+  | PageChanged(Page.Loading), _ -> { model with Page = Page.Loading }, Cmd.none
+
+let view model dispatch = concat {
+  Layout.Header.view dispatch
+
+  div {
+    attr.``class`` "container-md"
+
+    match model.Page with
+    | Page.Loading -> div { text "Loading..." }
+    | Page.Home -> div {
+        text "Home"
+
+        a {
+          attr.href "/presets"
+
+          text "Presets"
+        }
+      }
+  }
+}
+
+type App() =
+  inherit ProgramComponent<Model, Message>()
+
+  let defaultModel =
+    function
+    | Page.Home -> ()
+    | Page.Loading -> ()
+
+  override this.Program =
+    let router = Router.inferWithModel PageChanged _.Page defaultModel
+
+    Program.mkProgram (fun _ -> initModel) update view
+    |> Program.withConsoleTrace
+    |> Program.withRouter router
