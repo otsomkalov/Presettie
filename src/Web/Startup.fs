d90da6c08eccc3d5555fd2a6193c6779cb7cca3d
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-08-03 21:51:11 +0300

    Add auth to Web

--- src/Web/Startup.fs
+++ src/Web/Startup.fs
@@ -1,9 +1,17 @@
 ﻿module Web.Startup
 
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Configuration
 
 let builder = WebAssemblyHostBuilder.CreateDefault()
+
+builder.Services.AddOidcAuthentication(fun options ->
+
+  builder.Configuration.Bind("Oidc", options.ProviderOptions)
+
+  ())
 
 builder.Logging.SetMinimumLevel(LogLevel.Information)
 
