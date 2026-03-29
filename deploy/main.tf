f0b62b2e405138a697f3cede90c8de0ce775c2e2
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2026-03-29 13:31:20 +0300

    Move functions to Flex Consumption plan

--- deploy/main.tf
+++ deploy/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.38.1"
+      version = ">=4.54.0"
     }
   }
 }
@@ -29,9 +29,21 @@ resource "azurerm_resource_group" "rg-presettie" {
   tags = local.tags
 }
 
+resource "azurerm_log_analytics_workspace" "appi-ws-presettie" {
+  name                = "appi-ws-presettie-${var.env}"
+  location            = azurerm_resource_group.rg-presettie.location
+  resource_group_name = azurerm_resource_group.rg-presettie.name
+
+  sku               = "PerGB2018"
+  retention_in_days = 30
+
+  tags = local.tags
+}
+
 resource "azurerm_application_insights" "appi-presettie" {
   resource_group_name = azurerm_resource_group.rg-presettie.name
   location            = azurerm_resource_group.rg-presettie.location
+  workspace_id        = azurerm_log_analytics_workspace.appi-ws-presettie.id
 
   name             = "appi-presettie-${var.env}"
   application_type = "web"
@@ -45,14 +57,46 @@ resource "azurerm_storage_account" "st-presettie" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  identity {
+    type = "SystemAssigned"
+  }
+
   tags = local.tags
 }
 
+resource "azurerm_storage_container" "stc-bot-deployments-presettie" {
+  storage_account_id = azurerm_storage_account.st-presettie.id
+
+  name = "bot-deployments"
+}
+
+resource "azurerm_storage_container" "stc-api-deployments-presettie" {
+  storage_account_id = azurerm_storage_account.st-presettie.id
+
+  name = "api-deployments"
+}
+
 resource "azurerm_storage_queue" "stq-requests-presettie" {
-  storage_account_name = azurerm_storage_account.st-presettie.name
+  storage_account_id = azurerm_storage_account.st-presettie.id
 
   name = var.storage-queue-name
 }
+
+# Identity
+
+resource "azurerm_user_assigned_identity" "ado-pipeline-identity" {
+  location            = azurerm_resource_group.rg-presettie.location
+  name                = "ado-pipeline-identity-presettie-${var.env}"
+  resource_group_name = azurerm_resource_group.rg-presettie.name
+}
+
+resource "azurerm_role_assignment" "ado-pipeline-identity-blob-access" {
+  scope                = azurerm_storage_account.st-presettie.id
+  principal_id         = azurerm_user_assigned_identity.ado-pipeline-identity.principal_id
+  role_definition_name = "Storage Blob Data Contributor"
+}
+
+# App Service
 
 resource "azurerm_service_plan" "asp-presettie" {
   resource_group_name = azurerm_resource_group.rg-presettie.name
@@ -60,35 +104,36 @@ resource "azurerm_service_plan" "asp-presettie" {
 
   name     = "asp-presettie-${var.env}"
   os_type  = "Linux"
-  sku_name = "Y1"
+  sku_name = "FC1"
 
   tags = local.tags
 }
 
-resource "azurerm_linux_function_app" "func-presettie-bot" {
+resource "azurerm_function_app_flex_consumption" "func-presettie-bot" {
   resource_group_name = azurerm_resource_group.rg-presettie.name
   location            = azurerm_resource_group.rg-presettie.location
 
-  storage_account_name       = azurerm_storage_account.st-presettie.name
-  storage_account_access_key = azurerm_storage_account.st-presettie.primary_access_key
-  service_plan_id            = azurerm_service_plan.asp-presettie.id
+  service_plan_id = azurerm_service_plan.asp-presettie.id
 
   name = "func-presettie-bot-${var.env}"
 
-  functions_extension_version = "~4"
+  runtime_name    = "dotnet-isolated"
+  runtime_version = "9.0"
+
+  storage_authentication_type = "StorageAccountConnectionString"
+  storage_access_key          = azurerm_storage_account.st-presettie.primary_access_key
+  storage_container_endpoint  = "${azurerm_storage_account.st-presettie.primary_blob_endpoint}${azurerm_storage_container.stc-bot-deployments-presettie.name}"
+  storage_container_type      = "blobContainer"
+
+  instance_memory_in_mb  = 512
+  maximum_instance_count = 10
 
   identity {
     type = "SystemAssigned"
   }
 
   site_config {
-    application_insights_key = azurerm_application_insights.appi-presettie.instrumentation_key
-    app_scale_limit          = 10
-
-    application_stack {
-      dotnet_version              = "9.0"
-      use_dotnet_isolated_runtime = true
-    }
+    application_insights_connection_string = azurerm_application_insights.appi-presettie.connection_string
   }
 
   app_settings = merge(
@@ -109,30 +154,31 @@ resource "azurerm_linux_function_app" "func-presettie-bot" {
   tags = local.tags
 }
 
-resource "azurerm_linux_function_app" "func-presettie-api" {
+resource "azurerm_function_app_flex_consumption" "func-presettie-api" {
   resource_group_name = azurerm_resource_group.rg-presettie.name
   location            = azurerm_resource_group.rg-presettie.location
 
-  storage_account_name       = azurerm_storage_account.st-presettie.name
-  storage_account_access_key = azurerm_storage_account.st-presettie.primary_access_key
-  service_plan_id            = azurerm_service_plan.asp-presettie.id
+  service_plan_id = azurerm_service_plan.asp-presettie.id
 
   name = "func-presettie-api-${var.env}"
 
-  functions_extension_version = "~4"
+  runtime_name    = "dotnet-isolated"
+  runtime_version = "9.0"
+
+  storage_authentication_type = "StorageAccountConnectionString"
+  storage_access_key          = azurerm_storage_account.st-presettie.primary_access_key
+  storage_container_endpoint  = "${azurerm_storage_account.st-presettie.primary_blob_endpoint}${azurerm_storage_container.stc-api-deployments-presettie.name}"
+  storage_container_type      = "blobContainer"
+
+  instance_memory_in_mb  = 512
+  maximum_instance_count = 10
 
   identity {
     type = "SystemAssigned"
   }
 
   site_config {
-    application_insights_key = azurerm_application_insights.appi-presettie.instrumentation_key
-    app_scale_limit          = 10
-
-    application_stack {
-      dotnet_version              = "9.0"
-      use_dotnet_isolated_runtime = true
-    }
+    application_insights_connection_string = azurerm_application_insights.appi-presettie.connection_string
   }
 
   app_settings = merge(
